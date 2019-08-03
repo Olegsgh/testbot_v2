4ec4a54d563087c4884e709224c99274d7028ce6
oleg <olegsgh@yandex.ru>
2019-08-03 19:24:38 +0300

    add readme md

--- faq.py
+++ faq.py
@@ -1,27 +1,42 @@
-import tgalice
+import argparse
+import os
+import telebot
+from flask import Flask, request
+
+API_TOKEN = os.environ['TOKEN']
+bot = telebot.TeleBot(API_TOKEN)
+
+server = Flask(__name__)
+TELEBOT_URL = 'telebot_webhook/'
+BASE_URL = 'https://example-telebot.herokuapp.com/'
 
 
-TEXT_HELP = (
-    'Привет! Я бот, который умеет работать и в Телеграме и в Алисе.'
-    '\nЯ не умею делать примерно ничего, но могу с вами поздороваться.'
-    '\nКогда вам надоест со мной говорить, скажите "выход".'
-)
-TEXT_FAREWELL = 'Всего доброго! Если захотите повторить, скажите "Алиса, включи навык тест tgalice".'
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
 
 
-if __name__ == '__main__':
-    manager = tgalice.dialog_manager.CascadeDialogManager(
-        tgalice.dialog_manager.FAQDialogManager('faq.yaml', matcher='cosine'),
-        tgalice.dialog_manager.GreetAndHelpDialogManager(
-            greeting_message=TEXT_HELP,
-            help_message=TEXT_HELP,
-            default_message='Я вас не понимаю.',
-            exit_message='Всего доброго! Было приятно с вами пообщаться!'
-        )
-    )
-    connector = tgalice.dialog_connector.DialogConnector(
-        dialog_manager=manager,
-        storage=tgalice.session_storage.BaseStorage()
-    )
-    server = tgalice.flask_server.FlaskServer(connector=connector)
-    server.parse_args_and_run()
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+parser = argparse.ArgumentParser(description='Run the bot')
+parser.add_argument('--poll', action='store_true')
+args = parser.parse_args()
+
+if args.poll:
+    bot.remove_webhook()
+    bot.polling()
+else:
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.remove_webhook()
+    bot.set_webhook(url=BASE_URL + TELEBOT_URL + API_TOKEN)
