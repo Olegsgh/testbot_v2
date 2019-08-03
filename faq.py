f481d447c860e3d587864589cd1d38061790c859
oleg <olegsgh@yandex.ru>
2019-08-03 19:56:02 +0300

    add readme md

--- faq.py
+++ faq.py
@@ -35,6 +35,12 @@ def send_welcome(message):
     reply_with_log(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
 
 
+@bot.message_handler(commands=['сука', 'тварь'])
+def send_welcome(message):
+    reply_with_log(message, "Я предпочитаю более ласковое общение!")
+
+
+
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
