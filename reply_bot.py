5c0eeb13918b784c1a705a095eb43c784368be5f
Anton Ploskirev <ae.ploskirev@jet.su>
2019-09-01 14:54:26 +0300

    1. add gitignore 2. add test dataset 3. divide api and func

--- reply_bot.py
+++ reply_bot.py
@@ -1,16 +1,9 @@
-import argparse
 import os
-import telebot
+import re
+import csv
+
 from datetime import datetime
-from flask import Flask, request
 from pymongo import MongoClient
-
-API_TOKEN = os.environ['TOKEN']
-bot = telebot.TeleBot(API_TOKEN)
-
-server = Flask(__name__)
-TELEBOT_URL = 'telebot_webhook/'
-BASE_URL = 'https://testbot-heroku-v2.herokuapp.com/'
 
 MONGODB_URI = os.environ['MONGODB_URI']
 
@@ -18,6 +11,37 @@ mongo_client = MongoClient(MONGODB_URI)
 mongo_db = mongo_client.get_default_database()
 mongo_logs = mongo_db.get_collection('logs')
 
+def get_date(message):
+    return re.findall(r'\d{4}-\d{2}-\d{2}', message)
+
+def reply_full_week_report(message):
+    date = get_date(message)
+    reader = csv.DictReader("test_data.csv", delimiter=',')
+    count_row = 0
+    count_kredit = 0
+    for line in reader:
+        if (line["agbis_doc_date"] == date):
+            count_row += 1
+            count_kredit += count_kredit
+    return "За дату " + date + " было " + count_row + " покупок на сумму " + count_kredit
+
+def reply_kredit_week_report(message):
+    date = get_date(message)
+    reader = csv.DictReader("test_data.csv", delimiter=',')
+    count_kredit = 0
+    for line in reader:
+        if (line["agbis_doc_date"] == date):
+            count_kredit += count_kredit
+    return "За дату " + date + " было покупок на сумму " + count_kredit
+
+def reply_count_week_report(message):
+    date = get_date(message)
+    reader = csv.DictReader("test_data.csv", delimiter=',')
+    count_row = 0
+    for line in reader:
+        if (line["agbis_doc_date"] == date):
+            count_row += 1
+    return "За дату " + date + " было " + count_row + " покупок"
 
 def reply_with_log(message, response):
     mongo_logs.insert_one({
@@ -26,52 +50,9 @@ def reply_with_log(message, response):
         "user_nickname": message.from_user.username,
         "timestamp": datetime.utcnow()
     })
-    bot.reply_to(message, response)
+    return response
 
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    reply_with_log(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
-
-
-@bot.message_handler(commands=['suk'])
-def send_welcome(message):
-    reply_with_log(message, "Я предпочитаю более ласковое общение!")
-
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    reply_with_log(message, message.text)
-
-
-
-
-
-
-
-@server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=BASE_URL + TELEBOT_URL + API_TOKEN)
-    return "!", 200
-
-
-
-
-
-
-
-@server.route("/show_logs")
-def show_logs():
+def get_old_message():
     messages_list = list(mongo_logs.find())
     result = '<div>There are {} messages total. The last 10 are: </div><table>'.format(len(messages_list))
     row_template = '<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'
@@ -81,24 +62,4 @@ def show_logs():
             message['timestamp'], message['user_nickname'], message['text'], message['response']
         )
     result += '</table>'
-    return result, 200
-
-
-
-
-
-
-
-
-
-parser = argparse.ArgumentParser(description='Run the bot')
-parser.add_argument('--poll', action='store_true')
-args = parser.parse_args()
-
-if args.poll:
-    bot.remove_webhook()
-    bot.polling()
-else:
-    # webhook should be set first
-    webhook()
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    return result
