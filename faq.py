a078302a584acd1b891d61b9ed9240215ba65712
Anton Ploskirev <ae.ploskirev@jet.su>
2019-09-01 15:25:20 +0300

    revert rest-api to faq

--- faq.py
+++ faq.py
@@ -1,9 +1,9 @@
 import argparse
 import os
 import telebot
-from datetime import datetime
+import reply_bot
+
 from flask import Flask, request
-from pymongo import MongoClient
 
 API_TOKEN = os.environ['TOKEN']
 bot = telebot.TeleBot(API_TOKEN)
@@ -12,66 +12,40 @@ server = Flask(__name__)
 TELEBOT_URL = 'telebot_webhook/'
 BASE_URL = 'https://testbot-heroku-v2.herokuapp.com/'
 
-MONGODB_URI = os.environ['MONGODB_URI']
-
-mongo_client = MongoClient(MONGODB_URI)
-mongo_db = mongo_client.get_default_database()
-mongo_logs = mongo_db.get_collection('logs')
-
-
-def reply_with_log(message, response):
-    mongo_logs.insert_one({
-        "text": message.text,
-        "response": response,
-        "user_nickname": message.from_user.username,
-        "timestamp": datetime.utcnow()
-    })
-    bot.reply_to(message, response)
-
-
-# Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    reply_with_log(message, "Hi there, I am EchoBot. Just say anything nice and I'll say the exact same thing to you!")
+    response = reply_bot.reply_with_log(message, "Привет, можешь подписаться на новости, либо получить отчет.")
+    bot.reply_to(message, response)
 
-
-@bot.message_handler(commands=['suk'])
+@bot.message_handler(regexp="Отчёт \d{4}-\d{2}-\d{2}")
 def send_welcome(message):
-    reply_with_log(message, "Я предпочитаю более ласковое общение!")
+    response = reply_bot.reply_full_week_report(message)
+    bot.reply_to(message, response)
 
+@bot.message_handler(regexp="Отчёт \d{4}-\d{2}-\d{2} сумма")
+def send_welcome(message):
+    response = reply_bot.reply_kredit_week_report(message)
+    bot.reply_to(message, response)
 
-@bot.message_handler(commands=['sh'])
-def echo_message(message):
-    newmessages_list = list(mongo_logs.find())[-10]['text']
-    reply_with_log(message, newmessages_list)
+@bot.message_handler(regexp="Отчёт \d{4}-\d{2}-\d{2} сумма количество")
+def send_welcome(message):
+    response = reply_bot.reply_full_week_report(message)
+    bot.reply_to(message, response)
 
-
-@bot.message_handler(commands=['showme'])
-def echo_message(message):
-    reply_with_log(message, 'Будет функция')
-
-
-
-
-
-
+@bot.message_handler(regexp="Отчёт \d{4}-\d{2}-\d{2} количество")
+def send_welcome(message):
+    response = reply_bot.reply_count_week_report(message)
+    bot.reply_to(message, response)
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    reply_with_log(message, message.text)
-
-
-
-
-
-
+    reply_bot.reply_with_log(message, message.text)
 
 @server.route('/' + TELEBOT_URL + API_TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
-
 
 @server.route("/")
 def webhook():
@@ -79,32 +53,9 @@ def webhook():
     bot.set_webhook(url=BASE_URL + TELEBOT_URL + API_TOKEN)
     return "!", 200
 
-
-
-
-
-
-
 @server.route("/show_logs")
 def show_logs():
-    messages_list = list(mongo_logs.find())
-    result = '<div>There are {} messages total. The last 10 are: </div><table>'.format(len(messages_list))
-    row_template = '<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'
-    result += row_template.format('time', 'user', 'text from user', 'response from bot')
-    for message in messages_list[-100:]:
-        result += row_template.format(
-            message['timestamp'], message['user_nickname'], message['text'], message['response']
-        )
-    result += '</table>'
-    return result, 200
-
-
-
-
-
-
-
-
+    return reply_bot.get_old_message(), 200
 
 parser = argparse.ArgumentParser(description='Run the bot')
 parser.add_argument('--poll', action='store_true')
